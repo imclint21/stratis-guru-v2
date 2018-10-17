ebe8f8e8aa610e402b72259892b3a2d7124c08ad
Clint Mourlevat <clint.mourlevat@gmail.com>
2018-10-18 01:05:42 +0200

    Finishing autorefreshing with SignalR

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,48 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using Stratis.Guru.Models;
+
+namespace Stratis.Guru.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ApiController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        
+        [HttpGet]
+        [Route("price")]
+        public ActionResult<object> Price(bool stringify)
+        {
+            try
+            {
+                dynamic coinmarketcap = JsonConvert.DeserializeObject(_memoryCache.Get("Coinmarketcap").ToString());
+                if (stringify)
+                {
+                    return new TickerApi
+                    {
+                        UsdPrice = coinmarketcap.data.quotes.USD.price.ToString("C"),
+                        Last24Change = (coinmarketcap.data.quotes.USD.percent_change_24h / 100).ToString("P2")
+                    };
+                }
+                return new Ticker
+                {
+                    UsdPrice = coinmarketcap.data.quotes.USD.price,
+                    Last24Change = coinmarketcap.data.quotes.USD.percent_change_24h / 100
+                };
+            }
+            catch (Exception e)
+            {
+                //TODO: implement errors / logging
+                return null;
+            }
+        }
+    }
+}
---
+++ Models/TickerApi.cs
@@ -0,0 +1,8 @@
+namespace Stratis.Guru.Models
+{
+    public class TickerApi
+    {
+        public string UsdPrice { get; set; }
+        public string Last24Change { get; set; }
+    }
+}

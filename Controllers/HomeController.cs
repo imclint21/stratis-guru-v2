1d3c42323ea887e4f3cea4f6cffb3722cbad085c
Clint Mourlevat <clint.mourlevat@gmail.com>
2018-10-17 20:50:12 +0200

    Adding QrCode Action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using UnityEngine;
+using Color = System.Drawing.Color;
 
 namespace Stratis.Guru.Controllers
 {
@@ -30,6 +37,17 @@ namespace Stratis.Guru.Controllers
         public IActionResult Generator()
         {
             return View();
+        }
+
+        [Route("qr/{value}")]
+        public IActionResult Qr(string value)
+        {
+            var memoryStream = new MemoryStream();
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(value, QRCodeGenerator.ECCLevel.L);
+            var qrCode = new QRCode(qrCodeData);
+            qrCode.GetGraphic(20, Color.Black, Color.White, false).Save(memoryStream, ImageFormat.Png);
+            return File(memoryStream.ToArray(), "image/png");
         }
     }
 }

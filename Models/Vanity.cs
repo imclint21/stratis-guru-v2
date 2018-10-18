153252bbfbdb1ea9db81aed9ca95c120089ab618
Clint Mourlevat <clint.mourlevat@gmail.com>
2018-10-18 02:03:00 +0200

    Adding timestamp to vanity model

--- Models/Vanity.cs
+++ Models/Vanity.cs
@@ -1,9 +1,12 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Stratis.Guru.Models
 {
     public class Vanity
     {
+        public DateTime CreationDatetime { get; set; } = DateTime.Now;
+        
         [Required(ErrorMessage = "The prefix is required.")]
         [StringLength(4, MinimumLength = 1, ErrorMessage = "You connat overflow the size.")]
         public string Prefix { get; set; }

79984cd010cee40d7a8b053a30172ae4c7cf138f
Clint Mourlevat <clint.mourlevat@gmail.com>
2018-10-18 19:44:24 +0200

    Attempt to fix SignalR issue

--- Startup.cs
+++ Startup.cs
@@ -63,10 +63,10 @@ namespace Stratis.Guru
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 

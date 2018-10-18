c20cfa8a9414e3d397c628ee832d313adae0d8dd
Clint Mourlevat <clint.mourlevat@gmail.com>
2018-10-18 02:51:23 +0200

    Removing useless method

--- Hubs/UpdateHub.cs
+++ Hubs/UpdateHub.cs
@@ -5,9 +5,5 @@ namespace Stratis.Guru.Hubs
 {
     public class UpdateHub : Hub
     {
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
     }
 }

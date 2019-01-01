12d5dfab94357b8e59515b701ba1b217d5d11f7a
Clint.Network <me@clint.network>
2019-01-02 00:51:31 +0100

    Removing internal set

--- Stratis.Guru/Models/Participation.cs
+++ Stratis.Guru/Models/Participation.cs
@@ -10,8 +10,8 @@ namespace Stratis.Guru.Models
         public string Ticket { get; set; }
         public string WithdrawAddress { get; set; }
         public BsonDateTime CreationDate { get; set; }
-        public string Nickname { get; internal set; }
-        public string Draw { get; internal set; }
-        public double Amount { get; internal set; }
+        public string Nickname { get; set; }
+        public string Draw { get; set; }
+        public double Amount { get; set; }
     }
 }
